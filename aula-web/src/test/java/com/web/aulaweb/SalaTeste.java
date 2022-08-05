333ea804cfad1b40fdb05513bb98258cadd1ec7e
Rafael Leite <rafael.leite@solution4you.com.br>
2022-08-05 19:18:48 -0300

    ajuste mock

--- aula-web/src/test/java/com/web/aulaweb/SalaTeste.java
+++ aula-web/src/test/java/com/web/aulaweb/SalaTeste.java
@@ -2,6 +2,8 @@ package com.web.aulaweb;
 
 import com.web.aulaweb.domain.exception.DefaultException;
 import com.web.aulaweb.util.CamelCaseConver;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,6 +13,13 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 @SpringBootTest
 public class SalaTeste {
 
+    CamelCaseConver camel;
+
+    @BeforeEach
+    void setUp() {
+        camel = new CamelCaseConver();
+    }
+
     @Test
     public void criarUmaClasseParaConverterString(){
         CamelCaseConver camel = new CamelCaseConver();
@@ -18,26 +27,23 @@ public class SalaTeste {
 
     @Test
     public void converterStringParaCamelCase(){
-        CamelCaseConver camel = new CamelCaseConver();
+
         assertEquals("Rafael",camel.converter("rafael"));
     }
 
     @Test
     public void deveConverterStringsMisturadasMauisculoEMInusculo(){
-        CamelCaseConver camel = new CamelCaseConver();
         assertEquals("Rafael",camel.converter("raFael"));
     }
 
     @Test
     public void deveRetornarSeASalaTemMaisDe10Alunos(){
-        CamelCaseConver camel = new CamelCaseConver();
         assertTrue(camel.verificarRegraTotalAlunos(10));
     }
 
     @Test
     public void deveFalharAoVerificarRegraTotalDeALunosPorSala(){
         assertThrows(DefaultException.class, () -> {
-            CamelCaseConver camel = new CamelCaseConver();
             camel.lancarVerificacaoTotalALunosSala(9);
         } );
     }
