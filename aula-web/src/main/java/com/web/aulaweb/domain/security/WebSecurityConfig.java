36cd7846fbd4d697212aa564cc36f9b954be1a9d
Rafael Leite <rafael.leite@solution4you.com.br>
2022-08-08 16:43:06 -0300

    inclusao pom

---
+++ aula-web/src/main/java/com/web/aulaweb/domain/security/WebSecurityConfig.java
@@ -0,0 +1,32 @@
+package com.web.aulaweb.domain.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.httpBasic()
+			
+			.and()
+			.authorizeRequests()
+				.antMatchers(HttpMethod.GET, "/sala/**").permitAll()
+				.anyRequest().authenticated()
+	
+			.and()
+			.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				
+			.and()
+				.csrf().disable();
+	}
+	
+}
